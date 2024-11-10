8ea95a8b73e73235a74b7700441af3a3cd194edf
honda <honda.mafia@gmail.com>
2024-11-10 15:51:44 +0200

    ve3

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 function toggleSpoiler() {
   const spoilerContent = document.getElementById('videoSpoiler');
-  if (spoilerContent.style.display === 'none' || spoilerContent.style.display === '') {
-      spoilerContent.style.display = 'block';
-  } else {
-      spoilerContent.style.display = 'none';
-  }
+  spoilerContent.style.display = (spoilerContent.style.display === 'none' || spoilerContent.style.display === '') ? 'block' : 'none';
 }
